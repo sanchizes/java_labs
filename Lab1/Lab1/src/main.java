54abc1e760b732d3f320bf5e59246f6cd4329dbd
Oleksandr <78953309+sanchizes@users.noreply.github.com>
2023-01-17 00:29:06 +0200

    Update main.java

--- Lab1/Lab1/src/main.java
+++ Lab1/Lab1/src/main.java
@@ -1,31 +1,32 @@
-import java.util.Arrays;
 import java.util.Scanner;
 /*
 Author: Oleksandr Pavlyk
 Number in list: 61
 Variant: 1
-Task:Визначити ті рядки, довжина яких менша (більша) середньої.
+Task:Визначити ті рядки, довжина яких менша середньої.
 На вхід поступає масив String. На виході – масив String.
-Now hush little baby, don't you cry
-Everything's gonna be alright
-
-
-algorithm:
-    ввід тексту
-    розбити текст на рядки
-    визначення довжини рядків
-    знаходження середньої довжини рядків
-    порівнюємо довжини всіх рядків з середнім значенням
-    виводимо масив рядків довжина яких менша за середнє значення
  */
-public class main {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of lines: ");
         int wordCount = sc.nextInt();
         String[] text = new String[wordCount];
-        for (int i = 0; i < text.length; i++) {
-            text[i] = sc.next();
+        for (int i = 1; i <= text.length; i++) {
+            System.out.println("Enter line number " + i);
+            text[i - 1] = sc.next();
         }
-        System.out.println(Arrays.toString(text));
+        for (String string : text) {
+            if(string.length() < AverageLength(text)){
+                System.out.println(string);
+            }
+        }
+    }
+    public static int AverageLength(String[] text){
+        int sum = 0;
+        for (int i = 0; i < text.length; i++) {
+            sum += text[i].length();
+        }
+        return sum/ text.length;
     }
 }
